67c0362ef484fc6415e9eaf6047eeae084dc6089
Brent Grossman <brent.grossman@gastechnology.org>
2021-01-19 16:50:21 -0600

    First commit for remote

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+
+import WebMap from '@arcgis/core/WebMap';
+import MapView from '@arcgis/core/views/MapView';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss', 'mdcp_font_style.css', 'mdcp_gui_style.css'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(private zone: NgZone) { }
 
+  displayMessage = function (message) {
+
+    var div_content_message = document.getElementById("content-message");
+    div_content_message.classList.remove("open-content-message");
+    div_content_message.classList.add("open-content-message");
+    div_content_message.innerHTML = message;
+
+    setTimeout(function () {
+      div_content_message.classList.remove("open-content-message");
+
+    }, 6000);
+  }
+
+  initializeMap() {
+    const map = new WebMap({
+      basemap: "gray"
+    });
+
+    const view = new MapView({
+      container: "viewDiv",
+      map: map
+    });
+  }
+
+  ngOnInit(): any {
+    this.zone.runOutsideAngular(() => {
+      // Initialize MapView and return an instance of MapView
+      this.initializeMap();
+    });
+  }
 }
